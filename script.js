ad7659ff4c2796c84923ee9a5316aa01d0b19ecb
0W0rld <dmity3@gmail.com>
2025-11-24 20:13:22 +0300

    Откат

--- script.js
+++ script.js
@@ -100,7 +100,6 @@ document.getElementById("back-main").addEventListener("click",()=>{
   }
   document.getElementById("school").style.display="none";
   document.querySelectorAll("section").forEach(sec=>{ if(sec.id!=="school") sec.style.display="block"; });
-  // плавное появление всех блоков
   document.querySelectorAll('.fade').forEach(el=>el.classList.remove("show"));
   setTimeout(()=>{ document.querySelectorAll('.fade').forEach(el=>el.classList.add("show")); },50);
 });
